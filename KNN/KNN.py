fb52300aad9c0d76f162d68cda6306fde4a87796
namidairo777 <namidairo777@163.com>
2017-01-24 16:52:45 +0900

    handwriting test

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,4 +1,5 @@
 from numpy import *
+from os import listdir
 import operator
 
 def createDataSet():
@@ -52,9 +53,94 @@ def file2matrix(filename):
 		line = line.strip()
 		listFromLine = line.split('\t')
 		returnMat[index, :] = listFromLine[0:3]
-		classLabelVector.append(int(listFromLine[-1]))
+		classLabelVector.append(listFromLine[-1])
 		index += 1
 
 	return returnMat, classLabelVector
-	
 
+
+def autoNorm(dataSet):
+	'''
+	Normalization
+	dataSet: training sample
+	'''
+	minVals = dataSet.min(0)
+	maxVals = dataSet.max(0)
+	ranges = maxVals - minVals
+	normDataSet = zeros(shape(dataSet))
+	m = dataSet.shape[0]
+	normDataSet = dataSet - tile(minVals, (m, 1))
+	normDataSet = normDataSet/tile(ranges, (m, 1))
+	return normDataSet, ranges, minVals
+
+def datingClassTest():
+	'''
+	Test dating classifier
+	'''
+	hoRatio = 0.10
+	datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errotCount = 0.0
+	for i in range(numTestVecs):
+		classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+		if classifierResult != datingLabels[i]: errotCount += 1.0
+	print "Error rate is %f" % (errotCount/float(numTestVecs))	
+
+def classifyPerson():
+	'''
+	Classifier for person you would probably like
+	'''
+	resultList = ['not at all', 'in small doses', 'in large doses']
+	percentGames = float(raw_input("percentage of time spent playing video games?"))
+	ffMiles = float(raw_input("flier miles earned per year?"))
+	icecream = float(raw_input("liters of icecream consumed per year?"))
+	datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	inArr = array([ffMiles, percentGames, icecream])
+	classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
+	print "you will probably like this person:", resultList[int(classifierResult) - 1]
+
+def img2vector(filename):
+	'''
+	Image to vector
+	'''
+	returnVector = zeros((1, 1024))
+	fr = open(filename)
+	for i in range(32):
+		lineStr = fr.readline()
+		for j in range(32):
+			returnVector[0, 32*i+j] = int(lineStr[j])
+	return returnVector
+
+def handwritingClassTest():
+	'''
+	Classifier for handwriting
+	'''
+	hwLabels = []
+	trainingFileList = listdir('trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = zeros((m, 1024))
+	for i in range(m):
+		filenameStr = trainingFileList[i]
+		fileStr = filenameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i, :] = img2vector('trainingDigits/%s' % filenameStr)
+
+	testFileList = listdir('testDigits')
+	errotCount = 0.0
+	mTest = len(testFileList)
+
+	for i in range(mTest):
+		filenameStr = testFileList[i]
+		fileStr = filenameStr.split('.')[0]
+		classNumStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2vector('testDigits/%s' % filenameStr)
+		classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+
+		if classifierResult != classNumStr: errotCount += 1.0
+
+	print "total number of errors:%d" % errotCount
+	print "error rate: %f" % (errotCount/float(mTest))
